7e0fb530c597902b77a383f5134026aeb6af4600
godtime09 <113441213+godtime09@users.noreply.github.com>
2023-03-27 15:42:32 +0100

    Update view-post.php

--- view-post.php
+++ view-post.php
@@ -1,31 +1,32 @@
+<?php
+require_once('db.php');
+
+$stmt = $pdo->query("SELECT * FROM posts ORDER BY created_at DESC");
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>View Post</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+  <title>View Posts</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
-<div class="container">
-    <?php
-        // Connect to the database
-        require_once('db.php');
-        
-        // Retrieve the post from the database based on the id parameter
-        $id = $_GET['id'];
-        $stmt = $pdo->prepare('SELECT * FROM posts WHERE id = :id');
-        $stmt->execute(['id' => $id]);
-        $post = $stmt->fetch();
-        
-        if (!$post) {
-            echo '<p>Post not found</p>';
-        } else {
-            echo '<h1>' . htmlspecialchars($post['title']) . '</h1>';
-            echo '<p>By ' . htmlspecialchars($post['author']) . '</p>';
-            echo '<p>' . htmlspecialchars($post['content']) . '</p>';
-        }
-    ?>
-</div>
-
+  <div class="container">
+    <h1>View Posts</h1>
+    <div class="posts">
+      <?php foreach($posts as $post): ?>
+        <div class="post">
+          <h2><?php echo $post['title']; ?></h2>
+          <p><?php echo $post['content']; ?></p>
+          <?php if($post['image']): ?>
+            <img src="uploads/<?php echo $post['image']; ?>" alt="">
+          <?php endif; ?>
+          <p class="date"><?php echo $post['created_at']; ?></p>
+        </div>
+      <?php endforeach; ?>
+    </div>
+    <a href="submit-post.php" class="button">Add New Post</a>
+  </div>
 </body>
 </html>
